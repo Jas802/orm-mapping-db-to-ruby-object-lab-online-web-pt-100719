7a70b3225b167e0880f640f1d1b853b154de7909
Jas Spencer <jas.spencer414@gmail.com>
2020-01-02 16:59:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,8 +100,21 @@ def self.students_below_12th_grade
 
       DB[:conn].execute(sql, number).map do |row|
         self.new_from_db(row)
-      end.first
+      end
     end
+
+    def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
     def self.all_students_in_grade_X(grade)
     sql = <<-SQL
